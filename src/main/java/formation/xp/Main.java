96fa24e1167b5475b460afe21718c66a0274d4f8
CoreGend <corentin.gendreau@ensta-paris.fr>
2022-03-17 12:00:39 +0100

    Tests dans le main

--- src/main/java/formation/xp/Main.java
+++ src/main/java/formation/xp/Main.java
@@ -2,6 +2,32 @@ package formation.xp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("YOLOOOO");
+        Board board = new Board();
+        
+        board.print();
+        
+        System.out.println();
+        
+        board.putNumber(2, 1, 2);
+        
+        board.print();
+        
+        String input = null;
+        
+        input = Input.BoardInput();
+            
+        System.out.println(input);
+        
+        input = Input.BoardInput();
+        
+        System.out.println(input);
+        
+        input = Input.BoardInput();
+        
+        System.out.println(input);
+        
+        input = Input.BoardInput();
+        
+        System.out.println(input);
     }
 }
