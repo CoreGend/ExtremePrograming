da23ecf06ca945e917fcda5463a0f4afbb5acc3e
CoreGend <corentin.gendreau@ensta-paris.fr>
2022-03-17 20:10:24 +0100

    adding the generation of new tiles

--- src/main/java/formation/xp/Main.java
+++ src/main/java/formation/xp/Main.java
@@ -2,32 +2,10 @@ package formation.xp;
 
 public class Main {
     public static void main(String[] args) {
-        Board board = new Board();
         
-        board.print();
+        Game game = new Game();
+        game.init();
+        game.run();
         
-        System.out.println();
-        
-        board.putNumber(2, 1, 2);
-        
-        board.print();
-        
-        String input = null;
-        
-        input = Input.BoardInput();
-            
-        System.out.println(input);
-        
-        input = Input.BoardInput();
-        
-        System.out.println(input);
-        
-        input = Input.BoardInput();
-        
-        System.out.println(input);
-        
-        input = Input.BoardInput();
-        
-        System.out.println(input);
     }
 }
